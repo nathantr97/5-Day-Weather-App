dfad37017ba01234c6b0c4a25d1b5c4c1f63558b
Nathan <119708133+nathantr97@users.noreply.github.com>
2023-02-09 20:28:40 -0800

    added readme description

--- README.md
+++ README.md
@@ -1,12 +1,37 @@
 # Weather-Forecast-App
-The motivation behind this project is to utilize server-side APIs in order to create an application with 
+
 ## Description
 
+The motivation behind this project is to utilize server-side APIs in order to create an application with functional features. This project idea is to provide and display the current weather and forecast the next 5 days based on user inputs. Weather Forecast App was a great opportunity to practice fetching and pulling data from multiple API, and practicing the materials we covered in class. Furthermore, this application will solve the problem for any user who want to plan out their day ahead. 
 
 ## Installation
 
+<li> Create a repository on Github and git clone the files to computer </li>
+<li> Register API key from Openweathermap site and wait 2 hours for key to activate </li>
+<li> Open code in VS and be sure to follow git flow throughout the process to save and publish all files </li>
+<li> Run multiple trials to elimate and debug any errors </li>
+<li> Deploy webpage </li>
 
 ## Links
+
+<li> Github Link </li>
+
+
+
+<li> Application Link </li>
+
+
+## Usage
+
+<li> When webpage is launched, users will see a blank search bar with a placeholder says "example: Bellingham". <li>
+
+
+<li> Users can input any valid city around the world to see the current weather and the next 5-day forecast for the selected city. The information will include date, temperature, wind and humidity </li>
+
+
+
+<li> Users can continue to search for other cities and their previous searches will be placed under their most recent input </li>
+
 
 
 
--- assets/script.js
+++ assets/script.js
@@ -38,73 +38,72 @@ if (cityName === "") {
 // https://openweathermap.org/api/geocoding-api
 
 var geoURL="https://api.openweathermap.org/geo/1.0/direct?q=" + cityName + "&limit=" + limit + "&appid=" + APIKey;
-fetch(geoURL)
-    .then(function(response) {
-        return response.json();
+            fetch(geoURL)
+            .then(function(response) {
+            return response.json();
     })
-    .then(function(data1) {
-        console.log(data1);
-        var lat=data1[0].lat;
-        var lon=data1[0].lon;
-        console.log(lat, lon);
+            .then(function(data1) {
+          console.log(data1);
+          var lat=data1[0].lat;
+          var lon=data1[0].lon;
+          console.log(lat, lon);
 
  // https://openweathermap.org/current (this is for current city that user searched!)
 
 var weathermapURL= "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial" + "&appid=" + APIKey;
-fetch(weathermapURL)
-        .then(function(response2) {
-            return response2.json();
-        })
-        .then(function (data2) {
+        fetch(weathermapURL)
+            .then(function(response2) {
+        return response2.json();
+    })
+            .then(function (data2) {
             console.log(data2);
-            const usercityInput = data2.name;
-            const usercitySearch=document.createElement("h2");
-            usercitySearch.textContent= usercityInput + " " + todayDate;
-            citySearch.append(usercitySearch);
 
-            const userIcon = data2.weather[0].icon;
-            const userWeatherIcon= document.createElement("img");
-            userWeatherIcon.setAttribute("src", "http://openweathermap.org/img/w/" + userIcon + ".png");
+        const usercityInput = data2.name;
+        const usercitySearch=document.createElement("h2");
+        usercitySearch.textContent= usercityInput + " " + todayDate;
+        citySearch.append(usercitySearch);
 
-            citySearch.append(userWeatherIcon);
+        const userIcon = data2.weather[0].icon;
+        const userWeatherIcon= document.createElement("img");
+        userWeatherIcon.setAttribute("src", "http://openweathermap.org/img/w/" + userIcon + ".png");
+
+        citySearch.append(userWeatherIcon);
 
             // unicode for degree icon https://www.fileformat.info/info/unicode/char/b0/index.htm
 
             // temperature
-            const userTemp = data2.main.temp;
-            const usernewTemp=document.createElement("p");
-            usernewTemp.textContent=
-            "Temperature: " + userTemp + "\u00B0";
-            tempSearch.append(usernewTemp);
+        const userTemp = data2.main.temp;
+        const usernewTemp=document.createElement("p");
+        usernewTemp.textContent= "Temperature: " + userTemp + "\u00B0";
+        tempSearch.append(usernewTemp);
 
             // wind
-            const userWind=data2.wind.speed;
-            const usernewWind= document.createElement("p");
-            usernewWind.textContent="Wind:" + userWind + " MPH";
-            windSearch.append(usernewWind);
+        const userWind=data2.wind.speed;
+        const usernewWind= document.createElement("p");
+        usernewWind.textContent="Wind:" + userWind + " MPH";
+        windSearch.append(usernewWind);
 
             // humidity
-            const userHumidity=data2.main.humidity+ "%";
-            const usernewHumidity=document.createElement("p");
-            usernewHumidity.textContent=
-            "Humidity: " + userHumidity;
-            humiditySearch.append(usernewHumidity);
+        const userHumidity=data2.main.humidity+ "%";
+        const usernewHumidity=document.createElement("p");
+        usernewHumidity.textContent= "Humidity: " + userHumidity;
+        humiditySearch.append(usernewHumidity);
         });
 
         // functions and grabbing info for next 5 days forecast go here
         // https://openweathermap.org/forecast5
 
     var forecastURL="https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&units=imperial" + "&appid=" + APIKey;
-    fetch(forecastURL)
-    .then(function (response3) {
+        fetch(forecastURL)
+        .then(function (response3) {
         return response3.json();
     })
-    .then(function (data3) {
+        .then(function (data3) {
         
         // pulling the class forecast info for 5 days at footer
 
-        var fivedaysForecast = document.querySelectorAll(".forecast");
-        var dayArray = [0, 8, 16, 24, 32].map((i) => data3.list[i]);
+    var fivedaysForecast = document.querySelectorAll(".forecast");
+    var dayArray = [0, 8, 16, 24, 32].map((i) => data3.list[i]);
 
         //for loop to replace and display the data of 5 day forecast
         // still need to get tested to see if application displays 5 days info correctly
@@ -112,8 +111,8 @@ fetch(weathermapURL)
         for (let i = 0; i < dayArray.length; i++) {
             fivedaysForecast[i].textContent = "";
 
-            var newcastDate = new Date();
-            newcastDate.setDate(newcastDate.getDate() + (i + 1));
+    var newcastDate = new Date();
+        newcastDate.setDate(newcastDate.getDate() + (i + 1));
             newcastDate = newcastDate.toLocaleDateString("en-US");
 
             var fivedates = document.createElement("h2");
@@ -142,8 +141,8 @@ fetch(weathermapURL)
             forecastHumidity.textContent =
                 "Humidity: " + humidity5Day;
             fivedaysForecast[i].append(forecastHumidity);
-        }
+            }
+         });
     });
 });
 
-});
